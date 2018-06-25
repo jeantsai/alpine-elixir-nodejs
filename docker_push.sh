b837ab3284c33785fcaffbc41e884cd8529c3361
Jean Cai (jucai) <jucai@cisco.com>
2018-06-25 10:23:12 +0800

    Refactor docker repo names

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jeantsai/alpine-elixir-nodejs
+docker push jeantsai/elixir-node-alpine
+docker tag jeantsai/elixir-node-alpine jeantsai/phoenix-base-alpine
+docker push jeantsai/phoenix-base-alpine
